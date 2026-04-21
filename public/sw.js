8832259a88ebdb92202d37ed7e5daf0bc1c9f3f1
Aftonymous <aftabshk008@gmail.com>
2026-04-21 08:29:06 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -66,3 +66,4 @@ self.addEventListener('fetch', e => {
         }
     }));
 });
+// Note: This service worker is designed to be registered at the root scope ("/") to intercept requests to "/sw-video/*".
