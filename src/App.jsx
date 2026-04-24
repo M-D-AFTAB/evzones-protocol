0a767500ed03929a784a5faafb8953ec9bdee257
Nistha Dash <nisthapradeepdash@gmail.com>
2026-04-24 07:56:41 +0530

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import './App.css';
-import Login from './pages/Login';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import EvzonesStudio from './pages/EvzonesStudio';
+
 
 function App() {
-  return <Login />;
+  const [count, setCount] = useState(0)
+
+  return (
+    <EvzonesStudio />
+  )
 }
 
-export default App;
+export default App
